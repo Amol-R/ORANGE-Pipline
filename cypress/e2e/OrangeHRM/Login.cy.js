460fa67dbb64bbc3636a8ad67c40f5bc9151f290
Amol-R <amolrindhe.qa1@gmail.com>
2024-11-14 17:46:05 +0530

    Project created

---
+++ cypress/e2e/OrangeHRM/Login.cy.js
@@ -0,0 +1,15 @@
+///<reference types = 'cypress'/>
+import data from '../../fixtures/example.json'
+
+
+describe('Suit',()=>{
+
+    it('Login Page',()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('[name="username"]').type(data.Username)
+        cy.get('[name="password"]').type(data.Password)
+        cy.get('[type="submit"]').click()
+        cy.wait(2000)
+        cy.url().should('include','/dashboard/index')
+    })
+})
