0a44862c26349c92b27553f80526c92460c205e7
Amol-R <amolrindhe.qa1@gmail.com>
2024-11-14 20:48:27 +0530

    ok

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  env: {
+    login_url: 'https://www.google.com/',
+    
+  }, 
+
   e2e: {
+  baseUrl: "https://opensource-demo.orangehrmlive.com/web/index.php/auth",
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    
   },
 });
--- cypress/e2e/OrangeHRM/LoginFailde.cy.js
+++ cypress/e2e/OrangeHRM/LoginFailde.cy.js
@@ -4,12 +4,18 @@ import data from '../../fixtures/example.json'
 
 describe('Suit',()=>{
 
-    it('Login Page',()=>{
-        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    it('Login Page 01',()=>{
+        cy.visit('/login')
         cy.get('[name="username"]').type(data.Username)
-        cy.get('name="rrpassword"').type(data.Password)
+        cy.get('[name="password"]').type(data.Password)
         cy.get('[type="submit"]').click()
         cy.wait(2000)
-        cy.url().should('include','/dashboard/index11')
+        cy.url().should('include','/dashboard/index')
+    })
+
+    // Environment veriable
+    it.only('Google.com',()=>{
+        cy.visit(Cypress.env('login_url'))
+        
     })
 })
